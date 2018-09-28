8d7f76a0d0576bc77da586f56fb136735fe7c331
zhixuanziben <714919199@qq.com>
2018-09-28 11:15:15 +0800

    feat: 添加001 two-sum解题

---
+++ solutions/001.two-sum/two-sum.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    var i = 0;
+    var dict = {}
+    for(;i<nums.length;i++){
+        if(dict[nums[i]] !== undefined){
+            return [dict[nums[i]],i];
+        }else{
+            dict[target-nums[i]] = i;
+        }
+    }
+    return []
+};
